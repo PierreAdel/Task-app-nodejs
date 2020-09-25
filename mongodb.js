e46ad9aeadfef472d10d7b5a6faf60c61a130458
Pierre Adel <pierreadelkamel@gmail.com>
2020-09-25 18:20:43 +0200

    UPDATE operations

--- mongodb.js
+++ mongodb.js
@@ -18,40 +18,32 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
 
     const db = client.db(databaseName)
 
-//READ
+//UPDATE
 
-     db.collection('users').findOne({ _id: new ObjectID("5f6dd0021a0c372d9c455f8f")},(error,user) =>{
-        if(error)
-        {
-            return console.log("Unable to fetch")
+     db.collection('users').updateOne({
+        _id: new ObjectID("5f6dfbcc84ef5105149ed837")
+    }, {
+        $inc: {
+            age: -7
         }
-        console.log(user)
+
+    }).then((result) =>{
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
-    db.collection('users').find({age: 21}).toArray((error, users) =>
-    {
-        console.log(users)
-    })
-    db.collection('users').find({age: 21}).count((error, count) =>
-    {
-        console.log(count)
-    })
- 
-    db.collection('tasks').findOne({_id: new ObjectID("5f6dfd6f5f6a9c3154510857")}, (error, task) =>
-    {
-        if(error){
-           return console.log("Unable to fetch")
+
+    db.collection('tasks').updateMany({completed: false},{
+        $set: {
+            completed: true
         }
-        console.log(task)
-
-    })
-
-    db.collection('tasks').find({completed: false}).toArray((error, tasks) =>
+    }).then((result) =>
     {
-        if(error){
-            return console.log("Unable to fetch")
-         }
-         console.log(tasks)
+        console.log(result.modifiedCount)
+
+    }).catch((error) => {
+        console.log(error)
 
     })
 
-})
+}) 
