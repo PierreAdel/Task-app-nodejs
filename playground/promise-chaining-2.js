44da57c84e0a3437b77311d6e5420295e9ed4d04
Pierre Adel <pierreadelkamel@gmail.com>
2020-09-29 18:26:57 +0200

    used async/await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,7 +1,7 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-
+/* 
 Task.findByIdAndDelete("5f720d7faeddfb0bd8ca3f44").then((task) => {
     console.log("task removed. Title: "+task.description )
     return Task.countDocuments({completed: false})
@@ -9,4 +9,19 @@ Task.findByIdAndDelete("5f720d7faeddfb0bd8ca3f44").then((task) => {
     console.log(count)
 }).catch((e) => {
     console.log(e)
-}) 
+})  */
+
+const deleteTaskAndCount= async(id) => {
+
+    await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed: false})
+    return count
+
+} 
+
+deleteTaskAndCount("5f720dc5eee3d11978f4297f").then((count) =>{
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
+
+})
