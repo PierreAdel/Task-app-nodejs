3ecdf4e177bc683ebfe8c37b90a1982803f6137e
Pierre Adel <pierreadelkamel@gmail.com>
2020-10-02 18:02:11 +0200

    Authenticating Task Endpoints

--- src/models/user.js
+++ src/models/user.js
@@ -57,6 +57,7 @@ const userSchema = new mongoose.Schema({
     }]
 
 })
+
 userSchema.virtual('tasks', {
     ref: 'Task',
     localField: '_id',
--- src/routers/task.js
+++ src/routers/task.js
@@ -25,26 +25,30 @@ router.post('/tasks', auth, async (req,res) => {
 
 })
 
-router.get("/tasks", async (req,res) => {
+router.get("/tasks", auth, async (req,res) => {
 
 
     try {
-       const tasks = await Task.find({})
-       res.send(tasks)
+       //const tasks = await Task.find({owner: req.user._id})
+       // OR
+       await req.user.populate('tasks').execPopulate()
+     
+       res.send(req.user.tasks)
     } catch (e) {
-        res.status(500).send(e)
+        res.status(500).send()
     }
 
 })
 
-router.get("/tasks/:id", async (req,res) => {
+router.get("/tasks/:id", auth, async (req,res) => {
 
     const _id = req.params.id
+    
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({_id, owner: req.user._id})
         if(!task)
         {
-            return res.status(404).send("Task not found")
+            return res.status(404).send()
         }
         res.send(task)
 
@@ -54,7 +58,7 @@ router.get("/tasks/:id", async (req,res) => {
    
 })
 
-router.patch('/tasks/:id', async (req,res)=> {
+router.patch('/tasks/:id', auth, async (req,res)=> {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description','completed']
 
@@ -68,10 +72,8 @@ router.patch('/tasks/:id', async (req,res)=> {
     }
     try{
         const _id = req.params.id
-        const task = await Task.findById(_id)
-        updates.forEach((update) => task[update] = req.body[update])
-
-        task.save()
+        const task = await Task.findOne({_id, owner: req.user._id})
+        
         
         
         //const task = await Task.findByIdAndUpdate(_id, req.body, {new :true, runValidators:true}) 
@@ -79,6 +81,9 @@ router.patch('/tasks/:id', async (req,res)=> {
         {
             return res.status(404).send()
         }
+        updates.forEach((update) => task[update] = req.body[update])
+
+        task.save()
         res.send(task)
     }
     catch(e)
@@ -88,11 +93,11 @@ router.patch('/tasks/:id', async (req,res)=> {
     }
 })
 
-router.delete('/tasks/:id', async (req,res)=>{
+router.delete('/tasks/:id', auth, async (req,res)=>{
 
     const _id = req.params.id
     try {
-        const task = await Task.findByIdAndDelete(_id)
+        const task = await Task.findOneAndDelete({_id, owner: req.user._id})
         if(!task)
         {
             return res.status(404).send()
